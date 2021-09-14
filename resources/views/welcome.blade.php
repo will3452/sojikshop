f53e7dec413a794c91359a0d6af95bce479f56b6
william galas <williamgalas2@gmail.com>
2021-09-14 18:45:12 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<head>
+    <title>
+        Sojickshop
+    </title>
+    <style>
+        body,html {
+            margin: 0px;
+            padding: 0px;
+        }
+        .container {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            background: #30326E;
+        }
+        .container img {
+            width: 20vw;
+            height: 20vw;
+        }
+        h1{
+            font-family: sans-serif;
+            color: #B7FBE1;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <img src="/logo/square.png" alt="logo of shop">
+            
+        </div>
+        <h1>
+            UNDER DEVELOPMENT
+        </h1>
+    </div>
+</body>
