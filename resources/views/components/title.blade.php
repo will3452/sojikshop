53c905c990ccb3758c9670d433fa0be723379aee
william galas <williamgalas2@gmail.com>
2021-09-23 05:13:10 +0800

    fixed

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-    <p style="border-bottom:2px solid #222; font-size:1em;" class="d-inline">
+    <p style="border-bottom:2px solid #222; font-size:1.9em;" class="d-inline">
         {{ $slot }}
     </p>
 </div>
