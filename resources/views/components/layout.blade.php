d9daea5431e8ec62da6527e849426850e1ae7957
William Galas <36730333+will3452@users.noreply.github.com>
2021-11-15 04:01:06 +0800

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,11 +14,17 @@
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
     />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <style>
+        #main_content {
+            visibility: hidden;
+        }
+    </style>
 </head>
   <body class="">
     <x-goto-top></x-goto-top>
     @include('sweetalert::alert')
-    <div>
+    <div id="main_content">
         <x-navbar></x-navbar>
         {{ $slot }}
         {{-- <script>
