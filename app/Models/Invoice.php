ec6a26a63977c02ecc7392dd2dfdc4e48b48daa4
William Galas <36730333+will3452@users.noreply.github.com>
2021-11-18 13:32:42 +0800

    Update Invoice.php

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Invoice extends Model
