6827ccab476ae1992a3aa90c1a1a9190a0f2b061
William Galas <williamgalas2@gmail.com>
2021-12-01 13:43:32 +0800

    Update checkout-shipping-products.blade.php

--- resources/views/components/checkout-shipping-products.blade.php
+++ resources/views/components/checkout-shipping-products.blade.php
@@ -66,7 +66,7 @@
                         Total
                     </th>
                     <td class="p-2 border-2">
-                        PHP {{ $total != null ? number_format(($total + +($total * ((nova_get_setting('vat') ?? 12)/100))),2):'XXXX' }}
+                        PHP {{ $total != null ? number_format(($total + ($total * ((nova_get_setting('vat') ?? 12)/100))),2):'XXXX' }}
                     </td>
                 </tr>
                 <tr class="text-left">
